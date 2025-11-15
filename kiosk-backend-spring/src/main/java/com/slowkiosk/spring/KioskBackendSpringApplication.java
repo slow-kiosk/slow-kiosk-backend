973f67dec5e39e9f07f03641bb905d782c9d8b34
warrockhali <warrockhali@gmail.com>
2025-11-15 11:01:49 +0900

    프로젝트 초기화

---
+++ kiosk-backend-spring/src/main/java/com/slowkiosk/spring/KioskBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.slowkiosk.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskBackendSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskBackendSpringApplication.class, args);
+	}
+
+}
---
+++ kiosk-backend-spring/src/test/java/com/slowkiosk/spring/KioskBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slowkiosk.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskBackendSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
